9095240e166ad9892303b465e0f3869d823c20e9
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-23 21:59:23 -0500

    improving commenting style

--- FizzBuzzLib/GetFizzBuzz.cs
+++ FizzBuzzLib/GetFizzBuzz.cs
@@ -9,20 +9,15 @@ namespace FizzBuzzLib
             List<string> results = new List<string>();
             for (int number = input1; number <= input2; number++)
             {
-                //Console.WriteLine("{0}", i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : i.ToString())));
+                //Console.WriteLine("{0}", i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : i.ToString())));                
 
-                //intialize string variable to empty
-                string result = "";
-                //checking if divible by 3
-                if (number % 3 == 0) result = "Fizz";
-                //checking if diving by 5 and if divisible by 3 and 5 =FizzBuzz
-                if (number % 5 == 0) result = result + "Buzz";
-                //print number if not divisble by 3 or 5
-                if (result.Length == 0) result = number.ToString();
-                //Console.WriteLine(result);
-                results.Add(result);
+                string result = ""; //intialize string variable to store the result           
+                if (number % 3 == 0) result = "Fizz";  //checking if divible by 3                
+                if (number % 5 == 0) result = result + "Buzz"; //checking if diving by 5 and if divisible by 3 and 5 =FizzBuzz              
+                if (result.Length == 0) result = number.ToString();   //print number if not divisble by 3 or 5               
+                results.Add(result); //Console.WriteLine(result);
             }
-            return results;
+            return results; //returning List of string results
         }
     }
 }
